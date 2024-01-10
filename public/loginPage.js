5e43176d3d52d3b07535a7117243d397c03e5001
AnnaM <anna.horlach@gmail.com>
2024-01-10 09:31:52 +0300

    edits

--- public/loginPage.js
+++ public/loginPage.js
@@ -2,35 +2,22 @@
 
 const userForm = new UserForm();
 
-// Функция для запроса на авторизацию
-function loginFormAction(data) {
+userForm.loginFormCallback = function (data) {
   ApiConnector.login(data, (response) => {
     if (response.success) {
-      // Авторизация успешна, обновляем страницу
       location.reload();
     } else {
-      // Выводим ошибку в окно для ошибок
-      alert(response.error);
+      userForm.setLoginErrorMessage(response.error);
     }
   });
 }
 
-// Присваиваем функцию обратного вызова свойствам объекта UserForm для аторизации
-userForm.loginFormCallback = loginFormAction;
-
-
-// Функция для запроса на регистрацию
-function registerFormAction(data) {
+userForm.registerFormCallback = function (data) {
   ApiConnector.register(data, (response) => {
     if (response.success) {
-      // Регистрация успешна, обновляем страницу
       location.reload();
     } else {
-      // Выводим ошибку в окно для ошибок
-      alert(response.error);
+      userForm.setRegisterErrorMessage(response.error);
     }
   });
 }
-
-// Присваиваем функции обратного вызова свойствам объекта UserForm для регистрации
-userForm.registerFormCallback = registerFormAction;
